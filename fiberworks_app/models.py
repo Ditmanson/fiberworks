d81a29cdf31443ee071170cbbe938c6770e4f9e9
Travis Ditmanson <tdtimans@uccs.edu>
2023-12-17 18:53:58 -0700

    move to postgres

--- fiberworks_app/models.py
+++ fiberworks_app/models.py
@@ -22,13 +22,12 @@ class Product(models.Model):
         ('scarves', 'scarves'),
         ('other', 'other'),
     )
-    
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField(max_length=2000, blank=True, null=True)
     category = models.CharField(max_length=20, default='hats', choices=CATEGORY)
     tags = models.ManyToManyField(Tag, blank=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     in_stock = models.BooleanField(default=False)    
     on_homepage = models.BooleanField(default=False)
     def __str__(self):
@@ -50,6 +49,10 @@ class Homescreen(models.Model):
     # def get_absolute_url(self):
     #     return reverse("homescreen_details", kwargs={"pk": self.pk})
 
+class Logo(models.Model):
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    def __str__(self):
+        return self.image.name
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
